4b0c3d54f4c38f0b3b238c7f5289dac23a627b8f
irishmaiden18 <laurapohl18@gmail.com>
2026-02-19 15:08:42 -0500

    adds random dog breed info displayed on load

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const randomDogFactsList = document.querySelector("#random-dog-facts")
 const getNewDogFactsButton = document.querySelector("#get-new-dog-facts")
 
 const breedName = document.querySelector("#breed-name")
-const breedDscription = document.querySelector("#breed-description")
+const breedDescription = document.querySelector("#breed-description")
 const breedLifeSpan = document.querySelector("#breed-life-span")
 const breedMaleWeight = document.querySelector("#breed-male-weight")
 const breedFemaleWeight = document.querySelector("#breed-female-weight")
@@ -14,37 +14,50 @@ const breedHypoallergenic = document.querySelector("#breed-hypoallergenic")
 
 const changeDogBreedArea = document.querySelector("#change-dog-breed-area")
 
+//Variables
+let allDogBreeds = []
+let featuredDogBreed
+
 //Functions
+
+//initialize the page
+async function initialize() {
+    await getAllDogBreeds()
+    featuredDogBreed = getRandomDogBreed(allDogBreeds)
+    populateFeaturedBreedSection(featuredDogBreed)
+}
 
 //get an array of all dog breeds in the API
 async function getAllDogBreeds() {
     const response = await fetch(DOG_TEXT_URL + "breeds?page[size]=1000")
     const breedObj = await response.json()
-
     // console.log(breedObj)
-    const breedArray = breedObj.data
-    console.log(breedArray)
-    return breedArray
+    allDogBreeds = breedObj.data
+    console.log(allDogBreeds)
 }
 
 //get a random dog breed
 function getRandomDogBreed(array) {
-    console.log(array)
     const randomIndex = Math.floor(Math.random() * array.length)
-    console.log(randomIndex)
     console.log(array[randomIndex])
     return array[randomIndex]
 }
 
+//populate the featured dog breed section
+function populateFeaturedBreedSection(dogBreedObj) {
+    const breedAttributes = dogBreedObj.attributes
+    breedName.textContent = breedAttributes.name
+    breedDescription.textContent = breedAttributes.description
+    breedLifeSpan.textContent = `${breedAttributes.life.min} - ${breedAttributes.life.max} years`
+    breedMaleWeight.textContent = `${breedAttributes.male_weight.min} - ${breedAttributes.male_weight.max} lbs`
+    breedFemaleWeight.textContent = `${breedAttributes.female_weight.min} - ${breedAttributes.female_weight.max} lbs`
+    
+    if (breedAttributes.hypoallergenic) {
+        breedHypoallergenic.textContent = `${breedAttributes.name}s are hypoallergenic`
+    } else{
+        breedHypoallergenic.textContent = `${breedAttributes.name}s are NOT hypoallergenic`
+    }
+}
 
 
-
-
-
-
-
-
-
-const breedArray = getAllDogBreeds()
-console.log(breedArray)
-getRandomDogBreed(breedArray)
+initialize()
--- user_stories.md
+++ user_stories.md
@@ -10,7 +10,7 @@
 
 - at the bottom of the "Random Dog Facts" section will be a "Get New Dog Facts" button that generates new dog facts pulled from the API
 
-- on load, below "Random Dog Facts", "Featured Dog Breed" will display data on a randomly generated dog breed from the list at the API. Data will include: breed name, description, life span, male weight, female weight, & whether the dog is hypoallergenic
+- DONE -- on load, below "Random Dog Facts", "Featured Dog Breed" will display data on a randomly generated dog breed from the list at the API. Data will include: breed name, description, life span, male weight, female weight, & whether the dog is hypoallergenic
 
 - on load, below "Featured Dog Breed", will be the section titled "Change Dog Breed" followed by a list of buttons for the other possible dog breeds, pulled from the API
 
